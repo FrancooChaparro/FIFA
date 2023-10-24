83cf6a8c99670884be034036c4feba8cd6c35e02
FrancooChaparro <francoo_chaparro@hotmail.com>
2023-10-24 18:36:02 -0300

    work

---
+++ context/ListContext.tsx
@@ -0,0 +1,26 @@
+"use client"
+import { createContext, useContext } from 'react';
+
+export type MyContextType = {
+  Franco: (newValue: any[]) => void;
+  Gaston: (newValue: any[]) => void;
+  Marcos: (newValue: any[]) => void;
+  FrancoBombo: any[] | [];
+  GastonBombo: any[] | [];
+  MarcosBombo: any[] | [];
+  FrancoCopy: any[] | [];
+  GastonCopy: any[] | [];
+  MarcosCopy: any[] | [];
+};
+
+const MyContext = createContext<MyContextType | undefined>(undefined);
+
+export const useMyContext = () => {
+  const context = useContext(MyContext);
+  if (!context) {
+    throw new Error('useMyContext must be used within a MyProvider');
+  }
+  return context;
+};
+
+export default MyContext;
