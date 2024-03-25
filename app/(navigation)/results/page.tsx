dce8f4e9a25ba86cba9edd5f54a78603516b8e9b
FrancooChaparro <francoo_chaparro@hotmail.com>
2024-03-25 16:13:56 -0300

    rework

--- app/(navigation)/results/page.tsx
+++ app/(navigation)/results/page.tsx
@@ -1,56 +1,102 @@
-import React from 'react';
+import React from "react";
 import styles from "./results.module.css";
-import { ResultFinals } from '@/components/ResultFinals/ResultFinals';
-import { Nav } from '@/components/Nav/Nav';
-
+import { ResultFinals } from "@/components/ResultFinals/ResultFinals";
+import { Nav } from "@/components/Nav/Nav";
+import { Games, best_match, torneos } from "@/models/games";
 
 const Finals = () => {
+  const games: {
+    Player1: string;
+    Player2: string;
+    Result: string;
+    LocalEscudo: string;
+    LocalNombre: string;
+    LocalResultado: number;
+    VisitanteEscudo: string;
+    VisitanteNombre: string;
+    VisitanteResultado: number;
+  }[] = Games;
+
+  const Torneos: {
+    Player1: string;
+    Player2: string;
+    Result: string;
+    LocalEscudo: string;
+    LocalNombre: string;
+    LocalResultado: number;
+    VisitanteEscudo: string;
+    VisitanteNombre: string;
+    VisitanteResultado: number;
+  }[] = torneos;
+  const best_games: {
+    Player1: string;
+    Player2: string;
+    Result: string;
+    LocalEscudo: string;
+    LocalNombre: string;
+    LocalResultado: number;
+    VisitanteEscudo: string;
+    VisitanteNombre: string;
+    VisitanteResultado: number;
+  }[] = best_match;
   return (
     <>
-    <Nav /> 
-        <div className={styles.containerHeader}>
+      <Nav />
+      <div className={styles.containerHeader}>
         <div className={styles.containerMarcos}>MARCOS</div>
         <div className={styles.containerFranco}>FRANCO</div>
         <div className={styles.containerGaston}>GASTON</div>
-    </div>
-    <span className={styles.spansito}>Finals History</span>
-    <div className={styles.containerGames}>  
-    <ResultFinals Player1='F' Player2='M' Result="L" LocalEscudo={"/images/Ajax.png"} LocalNombre='AJAX' LocalResultado={3} VisitanteEscudo={"/images/Arsenal.jpg"} VisitanteNombre='ARSENAL' VisitanteResultado={0}/>
-    <ResultFinals Player1='F' Player2='G' Result="L" LocalEscudo={"/images/ManchesterUnited.png"} LocalNombre='MANCHESTER UNITED' LocalResultado={1} VisitanteEscudo={"/images/Juventus.jpg"} VisitanteNombre='JUVENTUS' VisitanteResultado={0}/>
-    <ResultFinals Player1='F' Player2='M' Result="L" LocalEscudo={"/images/Newcastle.png"} LocalNombre='NEWCASTLE' LocalResultado={1} VisitanteEscudo={"/images/Zenit.png"} VisitanteNombre='ZENIT' VisitanteResultado={0}/>
-    <ResultFinals Player1='F' Player2='M' Result="L" LocalEscudo={"/images/Liverpool.png"} LocalNombre='LIVERPOOL' LocalResultado={1} VisitanteEscudo={"/images/Tottenham.png"} VisitanteNombre='TOTTENHAM' VisitanteResultado={0}/>
-    <ResultFinals Player1='F' Player2='M' Result="NO" LocalEscudo={"/images/ParisSaintGermain.png"} LocalNombre='PARIS SAINT GERMAIN' LocalResultado={0} VisitanteEscudo={"/images/VillaReal.webp"} VisitanteNombre='VILLA REAL' VisitanteResultado={1}/>
-    <ResultFinals Player1='G' Player2='M' Result="L" LocalEscudo={"/images/BayernMunchen.png"} LocalNombre='BAYERN MUNICH' LocalResultado={2} VisitanteEscudo={"/images/RealMadrid.png"} VisitanteNombre='REAL MADRID' VisitanteResultado={2}/>
-    <ResultFinals Player1='G' Player2='M' Result="L" LocalEscudo={"/images/BayernMunchen.png"} LocalNombre='BAYERN MUNICH' LocalResultado={1} VisitanteEscudo={"/images/RealMadrid.png"} VisitanteNombre='REAL MADRID' VisitanteResultado={0}/>
-    <ResultFinals Player1='G' Player2='F' Result="L" LocalEscudo={"/images/Milan.png"} LocalNombre='MILAN' LocalResultado={2} VisitanteEscudo={"/images/ParisSaintGermain.png"} VisitanteNombre='PARIS SAINT GERMAIN' VisitanteResultado={4}/>
-
-    <ResultFinals Player1='F' Player2='G' Result="L" LocalEscudo={"/images/Liverpool.png"} LocalNombre='LIVERPOOL' LocalResultado={1} VisitanteEscudo={"/images/Juventus.jpg"} VisitanteNombre='JUVENTUS' VisitanteResultado={2}/>
-    <ResultFinals Player1='F' Player2='G' Result="L" LocalEscudo={"/images/ParisSaintGermain.png"} LocalNombre='PARIS SAINT GERMAIN' LocalResultado={2} VisitanteEscudo={"/images/Juventus.jpg"} VisitanteNombre='JUVENTUS' VisitanteResultado={0}/>
-    <ResultFinals Player1='G' Player2='M' Result="L" LocalEscudo={"/images/Arsenal.jpg"} LocalNombre='ARSENAL' LocalResultado={3} VisitanteEscudo={"/images/Internazionale.png"} VisitanteNombre='INTER' VisitanteResultado={0}/>
-    <ResultFinals Player1='F' Player2='M' Result="L" LocalEscudo={"/images/Barcelona.webp"} LocalNombre='BARCELONA' LocalResultado={0} VisitanteEscudo={"/images/Milan.png"} VisitanteNombre='MILAN' VisitanteResultado={0}/>
-    <ResultFinals Player1='G' Player2='M' Result="NO" LocalEscudo={"/images/Porto.jpg"} LocalNombre='PORTO' LocalResultado={2} VisitanteEscudo={"/images/Liverpool.png"} VisitanteNombre='LIVERPOOL' VisitanteResultado={1}/>
-    <ResultFinals Player1='F' Player2='M' Result="NO" LocalEscudo={"/images/Internazionale.png"} LocalNombre='INTER' LocalResultado={3} VisitanteEscudo={"/images/Ajax.png"} VisitanteNombre='AJAX' VisitanteResultado={1}/>
-    <ResultFinals Player1='F' Player2='M' Result="L" LocalEscudo={"/images/Arsenal.jpg"} LocalNombre='ARSENAL' LocalResultado={0} VisitanteEscudo={"/images/Valencia.jpg"} VisitanteNombre='VALENCIA' VisitanteResultado={1}/>
-    <ResultFinals Player1='F' Player2='G' Result="L" LocalEscudo={"/images/Sevilla.webp"} LocalNombre='SEVILLA' LocalResultado={1} VisitanteEscudo={"/images/BayernMunchen.png"} VisitanteNombre='BAYERN MUNICH' VisitanteResultado={0}/>
-    <ResultFinals Player1='F' Player2='M' Result="L" LocalEscudo={"/images/ParisSaintGermain.png"} LocalNombre='PSG' LocalResultado={2}  VisitanteEscudo={"/images/Chelsea.png"} VisitanteNombre='CHELSEA' VisitanteResultado={1}/>
-    <ResultFinals Player1='G' Player2='M' Result="L" LocalEscudo={"/images/SSC_Neapel.svg.png"} LocalNombre='NAPOLI' LocalResultado={2} VisitanteEscudo={"/images/Zenit.png"} VisitanteNombre='ZENIT' VisitanteResultado={0}/>     
-    <ResultFinals Player1='F' Player2='M' Result="L" LocalEscudo={"/images/Tottenham.png"} LocalNombre='TOTTENHAM' LocalResultado={1} VisitanteEscudo={"/images/AstonVilla.png"} VisitanteNombre='ASTON VILLA' VisitanteResultado={0}/>
-    <ResultFinals Player1='G' Player2='M' Result="L" LocalEscudo={"/images/ManchesterCity.png"} LocalNombre='MAN. CITY' LocalResultado={1} VisitanteEscudo={"/images/ASRoma.png"} VisitanteNombre='ROMA' VisitanteResultado={0}/>
-    <ResultFinals Player1='F' Player2='M' Result="L" LocalEscudo={"/images/ManchesterUnited.png"} LocalNombre='MAN. UNITED' LocalResultado={3} VisitanteEscudo={"/images/Valencia.jpg"} VisitanteNombre='VALENCIA' VisitanteResultado={2}/>
-    <ResultFinals Player1='F' Player2='G' Result="L" LocalEscudo={"/images/ParisSaintGermain.png"} LocalNombre='PSG' LocalResultado={0} VisitanteEscudo={"/images/AstonVilla.png"} VisitanteNombre='ASTON VILLA' VisitanteResultado={0}/>
-    <ResultFinals Player1='F' Player2='G' Result="L" LocalEscudo={"/images/Sevilla.webp"} LocalNombre='SEVILLA' LocalResultado={1} VisitanteEscudo={"/images/ManchesterCity.png"} VisitanteNombre='MAN. CITY' VisitanteResultado={2}/>
-    <ResultFinals Player1='F' Player2='G' Result="L" LocalEscudo={"/images/ParisSaintGermain.png"} LocalNombre='PSG' LocalResultado={3} VisitanteEscudo={"/images/Juventus.jpg"} VisitanteNombre='JUVENTUS' VisitanteResultado={1}/>
-    <ResultFinals Player1='F' Player2='G' Result="L" LocalEscudo={"/images/BayernMunchen.png"} LocalNombre='BAYERN MUNICH' LocalResultado={2} VisitanteEscudo={"/images/RealMadrid.png"} VisitanteNombre='REAL MADRID' VisitanteResultado={1}/>
-    <ResultFinals Player1='G' Player2='M' Result="L" LocalEscudo={"/images/RealMadrid.png"} LocalNombre='REAL MADRID' LocalResultado={3} VisitanteEscudo={"/images/Internazionale.png"} VisitanteNombre='INTER' VisitanteResultado={0}/>
-
- </div> 
- <span className={styles.spansito}>Popular Matchs</span>
- <div className={styles.containerGames}>  
-            <ResultFinals Player1='F' Player2='M' Result="L" LocalEscudo={"/images/SportingLisboa.png"} LocalNombre='SPORTING' LocalResultado={5} VisitanteEscudo={"/images/SanLorenzo.png"} VisitanteNombre='SAN LORENZO' VisitanteResultado={4}/>
-            <ResultFinals Player1='F' Player2='M' Result="L" LocalEscudo={"/images/ParisSaintGermain.png"} LocalNombre='PSG' LocalResultado={3} VisitanteEscudo={"/images/Chelsea.png"} VisitanteNombre='CHELSEA' VisitanteResultado={2}/>
-            </div> 
- </>
-  )
-}
+      </div>
+      <span className={styles.spansito}>Finals History</span>
+      <div className={styles.containerGames}>
+        {games.map((game) => (
+          <ResultFinals
+            Player1={game.Player1}
+            Player2={game.Player2}
+            Result={game.Result}
+            LocalEscudo={game.LocalEscudo}
+            LocalNombre={game.LocalNombre}
+            LocalResultado={game.LocalResultado}
+            VisitanteEscudo={game.VisitanteEscudo}
+            VisitanteNombre={game.VisitanteNombre}
+            VisitanteResultado={game.VisitanteResultado}
+          />
+        ))}
+      </div>
+      <span className={styles.spansito}>Torneos No oficiales</span>
+      <div className={styles.containerGames}>
+        {Torneos.map((game) => (
+          <ResultFinals
+            Player1={game.Player1}
+            Player2={game.Player2}
+            Result={game.Result}
+            LocalEscudo={game.LocalEscudo}
+            LocalNombre={game.LocalNombre}
+            LocalResultado={game.LocalResultado}
+            VisitanteEscudo={game.VisitanteEscudo}
+            VisitanteNombre={game.VisitanteNombre}
+            VisitanteResultado={game.VisitanteResultado}
+          />
+        ))}
+      </div>
+      <span className={styles.spansito}>Popular Matchs</span>
+      <div className={styles.containerGames}>
+        {best_games.map((game) => (
+          <ResultFinals
+            Player1={game.Player1}
+            Player2={game.Player2}
+            Result={game.Result}
+            LocalEscudo={game.LocalEscudo}
+            LocalNombre={game.LocalNombre}
+            LocalResultado={game.LocalResultado}
+            VisitanteEscudo={game.VisitanteEscudo}
+            VisitanteNombre={game.VisitanteNombre}
+            VisitanteResultado={game.VisitanteResultado}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default Finals;
--- components/Ranking/Ranking.tsx
+++ components/Ranking/Ranking.tsx
@@ -8,39 +8,39 @@ export const Ranking = () => {
         <div className={styles.containerTableGreen}>
         <Picture out={true} name={"PARIS SAIINT GERMAIN"} titles={4} img={"/images/ParisSaintGermain.png"} rank={1}/>
         <Picture out={true} name={"BAYERN MUNICH"} titles={3} img={"/images/BayernMunchen.png"} rank={2}/>
-        <Picture out={true} name={"MANCHESTER CITY"} titles={2} img={"/images/ManchesterCity.png"} rank={3}/>
-        <Picture out={true} name={"REAL MADRID"} titles={2} img={"/images/RealMadrid.png"} rank={4}/>
-        <Picture out={true} name={"MANCHESTER UNITED"} titles={2} img={"/images/ManchesterUnited.png"} rank={5}/>
-        <Picture out={true} name={"ASTON VILLA"} titles={1} img={"/images/AstonVilla.png"} rank={6}/>
-        <Picture out={true} name={"TOTTENHAM"} titles={1} img={"/images/Tottenham.png"} rank={7}/>
-        <Picture out={true} name={"NAPOLI"} titles={1} img={"/images/SSC_Neapel.svg.png"} rank={8}/>
-        <Picture out={true} name={"SEVILLA"} titles={1} img={"/images/Sevilla.webp"} rank={9}/>
-        <Picture out={true} name={"VALENCIA"} titles={1} img={"/images/Valencia.jpg"} rank={10}/>
+        <Picture out={true} name={"MANCHESTER UNITED"} titles={3} img={"/images/ManchesterUnited.png"} rank={3}/>
+        <Picture out={true} name={"MANCHESTER CITY"} titles={3} img={"/images/ManchesterCity.png"} rank={4}/>
+        <Picture out={true} name={"REAL MADRID"} titles={3} img={"/images/RealMadrid.png"} rank={5}/>
+        <Picture out={true} name={"BARCELONA"} titles={2} img={"/images/Barcelona.webp"} rank={6}/>
+        <Picture out={true} name={"TOTTENHAM"} titles={2} img={"/images/Tottenham.png"} rank={7}/>
+        <Picture out={true} name={"NEWCASTLE"} titles={2} img={"/images/Newcastle.png"} rank={8}/>
+        <Picture out={true} name={"PORTO"} titles={2} img={"/images/Porto.jpg"} rank={9}/>
+        <Picture out={true} name={"ASTON VILLA"} titles={1} img={"/images/AstonVilla.png"} rank={10}/>
         </div>
 
         <div className={styles.containerTableYellow}>
-        <Picture out={true} name={"INTER"} titles={1} img={"/images/Internazionale.png"} rank={11}/>
-        <Picture out={true} name={"PORTO"} titles={1} img={"/images/Porto.jpg"} rank={12}/>
-        <Picture out={true} name={"BARCELONA"} titles={1} img={"/images/Barcelona.webp"} rank={13}/>
-        <Picture out={true} name={"ARSENAL"} titles={1} img={"/images/Arsenal.jpg"} rank={14}/>
-        <Picture out={true} name={"JUVENTUS"} titles={1} img={"/images/Juventus.jpg"} rank={15}/>
-        <Picture out={true} name={"VILLA REAL"} titles={1} img={"/images/VillaReal.webp"} rank={16}/>
-        <Picture out={true} name={"LIVERPOOL"} titles={1} img={"/images/Liverpool.png"} rank={17}/>
-        <Picture out={true} name={"NEWCASTLE"} titles={1} img={"/images/Newcastle.png"} rank={18}/>
+        <Picture out={true} name={"NAPOLI"} titles={1} img={"/images/SSC_Neapel.svg.png"} rank={11}/>
+        <Picture out={true} name={"SEVILLA"} titles={1} img={"/images/Sevilla.webp"} rank={12}/>
+        <Picture out={true} name={"VALENCIA"} titles={1} img={"/images/Valencia.jpg"} rank={13}/>
+        <Picture out={true} name={"INTER"} titles={1} img={"/images/Internazionale.png"} rank={14}/>
+        <Picture out={true} name={"ARSENAL"} titles={1} img={"/images/Arsenal.jpg"} rank={15}/>
+        <Picture out={true} name={"JUVENTUS"} titles={1} img={"/images/Juventus.jpg"} rank={16}/>
+        <Picture out={true} name={"VILLA REAL"} titles={1} img={"/images/VillaReal.webp"} rank={17}/>
+        <Picture out={true} name={"LIVERPOOL"} titles={1} img={"/images/Liverpool.png"} rank={18}/>
         <Picture out={true} name={"AJAX"} titles={1} img={"/images/Ajax.png"} rank={19}/>
-        <Picture out={true} name={"ZENIT"} titles={0} img={"/images/Zenit.png"} rank={20}/>
+        <Picture out={true} name={"CHELSEA"} titles={0} img={"/images/Chelsea.png"} rank={20}/>
         </div>
 
         <div className={styles.containerTableRed}>
-        <Picture out={true} name={"AC MILAN"} titles={0} img={"/images/Milan.png"} rank={21}/>
-        <Picture out={true} name={"BORUSSIA DORTMUND"} titles={0} img={"/images/BorussiaDortmund.png"} rank={22}/>
-        <Picture out={true} name={"SPORTING LISBOA"} titles={0} img={"/images/SportingLisboa.png"} rank={23}/>
-        <Picture out={true} name={"LAZIO"} titles={0} img={"/images/Lazio.png"} rank={24}/>
-        <Picture out={true} name={"EVERTON"} titles={0} img={"/images/Everton.png"} rank={25}/>
-        <Picture out={true} name={"AS ROMA"} titles={0} img={"/images/ASRoma.png"} rank={26}/>
-        <Picture out={true} name={"BENFICA"} titles={0} img={"/images/Benfica.png"} rank={27}/>
-        <Picture out={true} name={"ATL. MADRID"} titles={0} img={"/images/AtleticoMadrid.png"} rank={28}/>
-        <Picture out={true} name={"CHELSEA"} titles={0} img={"/images/Chelsea.png"} rank={29}/>
+        <Picture out={true} name={"ZENIT"} titles={0} img={"/images/Zenit.png"} rank={21}/>
+        <Picture out={true} name={"AC MILAN"} titles={0} img={"/images/Milan.png"} rank={22}/>
+        <Picture out={true} name={"BORUSSIA DORTMUND"} titles={0} img={"/images/BorussiaDortmund.png"} rank={23}/>
+        <Picture out={true} name={"SPORTING LISBOA"} titles={0} img={"/images/SportingLisboa.png"} rank={24}/>
+        <Picture out={true} name={"LAZIO"} titles={0} img={"/images/Lazio.png"} rank={25}/>
+        <Picture out={true} name={"ATL. MADRID"} titles={0} img={"/images/AtleticoMadrid.png"} rank={26}/>
+        <Picture out={true} name={"EVERTON"} titles={0} img={"/images/Everton.png"} rank={27}/>
+        <Picture out={true} name={"AS ROMA"} titles={0} img={"/images/ASRoma.png"} rank={28}/>
+        <Picture out={true} name={"BENFICA"} titles={0} img={"/images/Benfica.png"} rank={29}/>
         <Picture out={true} name={"WOLFSBURGO"} titles={0} img={"/images/wolfburgo.png"} rank={30}/>
         <Picture out={false} name={"FIORENTINA"} titles={0} img={"/images/fiorentina.png"} rank={31}/>
         <Picture out={false} name={"LYON"} titles={0} img={"/images/Lyon.png"} rank={32}/>
