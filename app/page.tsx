bbc99d2f88c7d4928e04386f922602cdd4e4933b
FrancooChaparro <francoo_chaparro@hotmail.com>
2024-04-01 16:48:27 -0300

    res

--- app/page.tsx
+++ app/page.tsx
@@ -1,61 +1,14 @@
-"use client";
-import { ContainerNews } from "@/components/ContainerNews/ContainerNews";
 import styles from "./page.module.css";
 import Banner from "@/components/Banner/Banner";
-import { News } from "@/components/News/News";
+import { ContainerNews } from "@/components/ContainerNews/ContainerNews";
 import { Ranking } from "@/components/Ranking/Ranking";
-import { useRef } from "react";
 
 export default function Home() {
-  // const carouselRef = useRef<HTMLDivElement>(null);
-  // const izq = "<";
-  // const handleScrollLeft = () => {
-  //   if (carouselRef.current) {
-  //     carouselRef.current.scrollBy({
-  //       left: -425,
-  //       behavior: "smooth",
-  //     });
-  //   }
-  // };
-  // const handleScrollRight = () => {
-  //   if (carouselRef.current) {
-  //     carouselRef.current.scrollBy({
-  //       left: 425,
-  //       behavior: "smooth",
-  //     });
-  //   }
-  // };
+
   return (
     <main className={styles.main}>
       <Banner />
       <ContainerNews />
-      
-      {/* <div className={styles.container_news}>
-        <div className={styles.container_news_header}>
-          <h1>BREAKING NEWS</h1>
-          <div className={styles.container_buttons}>
-            <button
-              className={styles.buttons}
-              onClick={() => handleScrollLeft()}
-            >
-              <div className={styles.icons}>
-                <img src="/icon.svg" alt="home" />
-              </div>
-            </button>
-            <button
-              className={styles.buttons}
-              onClick={() => handleScrollRight()}
-            >
-              <div className={styles.icons}>
-                <img src="/icon2.svg" alt="home" />
-              </div>
-            </button>
-          </div>
-        </div>
-        <div ref={carouselRef} className={styles.container_news_footer}>
-          <News />
-        </div>
-      </div> */}
       <Ranking />
     </main>
   );
